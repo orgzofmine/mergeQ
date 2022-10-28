d75925aa5de57e5c02501c87ca29add54b32185c
satishbraju <40523115+satishbraju@users.noreply.github.com>
2022-10-28 15:57:51 +0200

    Create dummy.py

---
+++ dummy.py
@@ -0,0 +1,6 @@
+import re
+#a = re.split(r'(/)', sttxtr)
+#a = re.search(r'\d{11,12}', sttxtr)
+dat_re = re.msearch(r'\d{11,12')
+dat_list = [dat_re.search(x).group() for x in l if dat_re.search(x)]
+print(dat_list)
